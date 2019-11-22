818671da384a171ad3f16d8c82b439c5d68d7182
Kamil Dudka <kdudka@redhat.com>
2019-11-22 16:40:20 +0100

    tests/single-c/predator/predator-regre-test-0006: new test

---
+++ tests/single-c/predator/predator-regre-test-0006/test-0006.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+
+typedef void *TItem;
+
+static void dispose_list(TItem **list) {
+    TItem *item;
+    TItem **list_var = list;
+
+    while ((item = *list)) {
+        TItem *next = (TItem *) *item;
+        free(item);
+        item = next;
+        list = (TItem **) &item;
+    }
+    *list_var = NULL;
+}
+
+int main() {
+    TItem *list =  malloc(sizeof(TItem));
+    *list = malloc(sizeof(TItem));
+    *((TItem *) *list) = NULL;
+    dispose_list(&list);
+    dispose_list(&list);
+    return 0;
+}
+
+/**
+ * @file test-0006.c
+ *
+ * @brief another variant of two nodes long SLL destruction
+ *
+ * - also reports problems caused by unhandled OOM state
+ * - silent in "fast" mode
+ */
