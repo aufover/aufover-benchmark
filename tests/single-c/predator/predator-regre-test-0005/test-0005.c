a955c37823ef59efcedf0e20b4ee1b82850c3631
Kamil Dudka <kdudka@redhat.com>
2019-11-22 16:36:29 +0100

    tests/single-c/predator/predator-regre-test-0005: new test

---
+++ tests/single-c/predator/predator-regre-test-0005/test-0005.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+typedef void *TItem;
+
+int main() {
+    TItem *list = malloc(sizeof(TItem));
+    *list = malloc(sizeof(TItem));
+    *((TItem *) *list) = NULL;
+
+    while (list) {
+        TItem *item = list;
+        list = (TItem *) *list;
+        free(item);
+    }
+
+    return 0;
+}
+
+/**
+ * @file test-0005.c
+ *
+ * @brief non-recursive destruction of a SLL that consists of two nodes
+ *
+ * - OOM state is not handled properly and the analysis detects a
+ *   dereference of NULL value and possible junk as the consequence
+ *   of it
+ *
+ * - when analysed in "fast" mode, no flaws are detected
+ */
