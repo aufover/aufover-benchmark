262f743b5a26ad663c9828ea5535c76f79b3225d
Kamil Dudka <kdudka@redhat.com>
2019-11-22 16:17:36 +0100

    tests/single-c/predator/predator-regre-test-0004: new test

---
+++ tests/single-c/predator/predator-regre-test-0004/test-0004.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+int main() {
+    void **ptr = NULL;
+
+    while (1) {
+        void **data = ptr;
+        ptr = malloc(sizeof *ptr);
+        free(ptr);
+        ptr = data;
+    }
+
+    return 0;
+}
+
+/**
+ * @file test-0004.c
+ *
+ * @brief infinite loop, one malloc() and one free() per each iteration
+ *
+ * - the analysis stops, though the program is infinite
+ * - there is no junk and no memory error possible
+ * - Predator issues a warning:
+ *     "end of function main() has not been reached"
+ */
