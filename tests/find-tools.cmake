0c48144e226a9e46550880aa4de39f667183d285
Kamil Dudka <kdudka@redhat.com>
2020-04-01 16:34:15 +0200

    add support and coverage for predator

--- tests/find-tools.cmake
+++ tests/find-tools.cmake
@@ -19,11 +19,17 @@ endmacro()
 # in-memory C program with empty main()
 set(empty_main "<(echo 'int main(){}')")
 
+# in-memory C program with main() that may trigger an invalid dereference
+set(broken_main "<(echo 'int main() { void **p = *p; }')")
+
 # make it possible to override path to executables
 set(TOOLS_SUPPORTED gcc clang cppcheck cbmc divine symbiotic)
 foreach(tool ${TOOLS_SUPPORTED})
     set(TOOL_EXEC_${tool} ${tool} CACHE STRING "command used to run ${tool}")
 endforeach()
+set(TOOLS_SUPPORTED ${TOOLS_SUPPORTED} predator)
+set(TOOL_EXEC_predator "${TOOL_EXEC_gcc} -fplugin=predator"
+    CACHE STRING "command used to run predator")
 
 # probe for available static analyzers and formal verification tools
 append_tool_on_succ(gcc        "${TOOL_EXEC_gcc} -xc ${empty_main} -o /dev/null")
@@ -36,3 +42,5 @@ append_tool_on_succ(divine     "${TOOL_EXEC_divine} version >/dev/null 2>&1 && \
                                 test -x /usr/bin/divine2csgrep")
 append_tool_on_succ(symbiotic  "${TOOL_EXEC_symbiotic} --version >/dev/null 2>&1 && \
                                 test -x /usr/bin/symbiotic2cs")
+append_tool_on_succ(predator   "${TOOL_EXEC_predator} -xc ${broken_main} -o /dev/null \
+                                2>&1 | grep 'error: invalid dereference' >/dev/null")
