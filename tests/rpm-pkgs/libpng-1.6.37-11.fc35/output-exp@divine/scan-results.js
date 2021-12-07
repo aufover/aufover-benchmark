80beb2e5dc108cd1683513f3a3a1f9a2463e8b8d
Kamil Dudka <kdudka@redhat.com>
2021-12-07 15:56:55 +0100

    rpm-pkgs: add libpng-1.6.37-11.fc35

---
+++ tests/rpm-pkgs/libpng-1.6.37-11.fc35/output-exp@divine/scan-results.js
@@ -0,0 +1,19 @@
+{
+    "scan": {
+        "analyzer-version-divine": "4.4.2_4_gd47985e0b",
+        "analyzer-version-gcc": "11.2.1",
+        "enabled-plugins": "divine, gcc",
+        "exit-code": 1,
+        "host": "f34",
+        "known-false-positives": "/usr/share/csmock/known-false-positives.js",
+        "mock-config": "fedora-35-x86_64",
+        "project-name": "libpng-1.6.37-11.fc35",
+        "store-results-to": "/home/kdudka/git/aufover-benchmark/workdir-for-sync/rpm-pkgs/libpng-1.6.37-11.fc35/divine/output",
+        "time-created": "2021-12-07 15:31:27",
+        "time-finished": "2021-12-07 15:34:37",
+        "tool": "csmock",
+        "tool-args": "'/usr/bin/csmock' '-o' '/home/kdudka/git/aufover-benchmark/workdir-for-sync/rpm-pkgs/libpng-1.6.37-11.fc35/divine/output' '-f' '/home/kdudka/git/aufover-benchmark/workdir-for-sync/rpm-pkgs/libpng-1.6.37-11.fc35/libpng-1.6.37-11.fc35.src.rpm' '-r' 'fedora-35-x86_64' '-t' 'divine' '--divine-timeout=8' '--keep-going'",
+        "tool-version": "csmock-3.2.0.20211207.150340.g263db49.csexec-1.fc34"
+    },
+    "defects": ""
+}
